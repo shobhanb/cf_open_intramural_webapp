dbf9c6ab161fbd086ad750184d450b84569a3562
Shobhan Bhatt <shobhanb@gmail.com>
2025-02-10 01:54:53 +0530

    App upgrades

--- app/athlete/service.py
+++ app/athlete/service.py
@@ -1,10 +1,12 @@
+from __future__ import annotations
+
 from typing import Any
 from uuid import UUID
 
-from sqlalchemy import select
+from sqlalchemy import select, update
 
 from app.athlete.models import Athlete
-from app.cf_games.constants import AFFILIATE_ID, YEAR
+from app.cf_games.constants import AFFILIATE_ID, TEAM_LEADER_MAP, YEAR
 from app.database.dependencies import db_dependency
 
 
@@ -21,7 +23,7 @@ async def get_year_affiliate_athletes(
 async def get_athlete_teams_list(
     db_session: db_dependency,
 ) -> list[dict[str, Any]]:
-    stmt = select(Athlete.name, Athlete.team_name, Athlete.team_leader).order_by(
+    stmt = select(Athlete.name, Athlete.competitor_id, Athlete.team_name, Athlete.team_leader).order_by(
         Athlete.team_name,
         Athlete.team_leader.desc(),
         Athlete.name,
@@ -49,3 +51,46 @@ async def get_athlete_teams_dict(
             teams[team_name] = [row]
 
     return teams
+
+
+async def assign_athlete_to_team(
+    db_session: db_dependency,
+    competitor_id: int,
+    team_name: str,
+) -> None:
+    athlete = await Athlete.find(async_session=db_session, competitor_id=competitor_id)
+    if athlete:
+        athlete.team_name = team_name
+        db_session.add(athlete)
+        await db_session.commit()
+
+
+async def assign_athlete_team_role(
+    db_session: db_dependency,
+    competitor_id: int,
+    tl_c: str,
+) -> None:
+    athlete = await Athlete.find(async_session=db_session, competitor_id=competitor_id)
+    if athlete:
+        athlete.team_leader = TEAM_LEADER_MAP.get(tl_c, 0)
+        db_session.add(athlete)
+        await db_session.commit()
+
+
+async def get_team_names(
+    db_session: db_dependency,
+) -> list[str]:
+    stmt = select(Athlete.team_name).distinct()
+    ret = await db_session.execute(stmt)
+    result = ret.scalars()
+    return list(result)
+
+
+async def rename_team(
+    db_session: db_dependency,
+    team_name_current: str,
+    team_name_new: str,
+) -> None:
+    stmt = update(Athlete).where(Athlete.team_name == team_name_current).values(team_name=team_name_new)
+    await db_session.execute(stmt)
+    await db_session.commit()
--- app/athlete/views.py
+++ app/athlete/views.py
@@ -1,10 +1,19 @@
+from __future__ import annotations
+
 import logging
 from typing import Annotated
 
 from fastapi import APIRouter, Form, Request, Response, status
 from fastapi.responses import HTMLResponse
 
-from app.athlete.service import get_athlete_teams_dict, get_athlete_teams_list
+from app.athlete.service import (
+    assign_athlete_team_role,
+    assign_athlete_to_team,
+    get_athlete_teams_dict,
+    get_athlete_teams_list,
+    get_team_names,
+    rename_team,
+)
 from app.auth.exceptions import unauthorised_exception
 from app.auth.service import authenticate_request
 from app.cf_games.constants import TEAM_LEADER_REVERSE_MAP
@@ -46,6 +55,38 @@ async def get_assign_teams_page(request: Request, db_session: db_dependency) -> 
     )
 
 
+@athlete_router.get("/rename_teams", response_class=HTMLResponse, status_code=status.HTTP_200_OK)
+async def get_rename_teams_page(request: Request, db_session: db_dependency) -> Response:
+    user = authenticate_request(request)
+    if not user:
+        raise unauthorised_exception()
+
+    teams = await get_team_names(db_session=db_session)
+    return templates.TemplateResponse(
+        request=request,
+        name="pages/rename_teams.jinja2",
+        context={
+            "teams": teams,
+        },
+    )
+
+
+@athlete_router.get("/auto_team_assign", response_class=HTMLResponse, status_code=status.HTTP_200_OK)
+async def get_auto_team_assign_page(request: Request, db_session: db_dependency) -> Response:
+    user = authenticate_request(request)
+    if not user:
+        raise unauthorised_exception()
+
+    teams = await get_athlete_teams_dict(db_session=db_session)
+    return templates.TemplateResponse(
+        request=request,
+        name="pages/auto_team_assign.jinja2",
+        context={
+            "teams": teams,
+        },
+    )
+
+
 @athlete_router.post("/athlete_teams", response_class=HTMLResponse, status_code=status.HTTP_200_OK)
 async def get_athlete_teams_partial(
     request: Request,
@@ -67,3 +108,53 @@ async def get_athlete_teams_partial(
             "athlete_teams": filtered_teams,
         },
     )
+
+
+@athlete_router.put("/assign_athlete_team/{competitor_id}", response_class=HTMLResponse, status_code=status.HTTP_200_OK)
+async def put_assign_athlete_teams(
+    request: Request,
+    competitor_id: int,
+    team_name: Annotated[str, Form()],
+    db_session: db_dependency,
+) -> Response:
+    user = authenticate_request(request)
+    if not user:
+        raise unauthorised_exception()
+
+    await assign_athlete_to_team(db_session=db_session, competitor_id=competitor_id, team_name=team_name)
+
+    return HTMLResponse(content=f"<td>{team_name}</td>")
+
+
+@athlete_router.put(
+    "/assign_athlete_team_leader/{competitor_id}",
+    response_class=HTMLResponse,
+    status_code=status.HTTP_200_OK,
+)
+async def put_assign_athlete_team_leader(
+    request: Request,
+    competitor_id: int,
+    tl_c: Annotated[str, Form()],
+    db_session: db_dependency,
+) -> HTMLResponse:
+    user = authenticate_request(request)
+    if not user:
+        raise unauthorised_exception()
+
+    await assign_athlete_team_role(db_session=db_session, competitor_id=competitor_id, tl_c=tl_c)
+
+    return HTMLResponse(content=f"<td>{tl_c}</td>")
+
+
+@athlete_router.put("/rename_team/{team_name_current}", response_class=HTMLResponse, status_code=status.HTTP_200_OK)
+async def put_rename_team(
+    request: Request,
+    team_name_current: str,
+    team_name_new: Annotated[str, Form()],
+    db_session: db_dependency,
+) -> None:
+    user = authenticate_request(request)
+    if not user:
+        raise unauthorised_exception()
+
+    await rename_team(db_session=db_session, team_name_current=team_name_current, team_name_new=team_name_new)
